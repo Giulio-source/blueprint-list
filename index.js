3c168fbaeb739dad287112f3eb00f44202c7894a
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-03-31 08:34:49 +0200

    deleted model

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Todo = require("./models/Todo");
 
-// mongoose.connect("mongodb://localhost/blogDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
-// const db = mongoose.connection;
-// db.on("error", (err) => console.log(err));
-// db.once("open", () => console.log("Connected to DB"));
-// mongoose.set("useFindAndModify", false);
+mongoose.connect("mongodb://localhost/trueblueDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to DB"));
+mongoose.set("useFindAndModify", false);
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
