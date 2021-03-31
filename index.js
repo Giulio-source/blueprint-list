0320a797b57700834c4b17fd4c9c2c69666f89ea
Giulio Poggia <giulio.poggia@gmail.com>
2021-03-31 15:22:23 +0200

    refactored sass

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var todos = [];
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
@@ -15,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", (req, res) => {
-  res.render("create", { todo: {} });
+  res.render("create");
 });
 
 app.post("/create", (req, res) => {
@@ -33,19 +32,15 @@ app.post("/create", (req, res) => {
 app.post("/delete", (req, res) => {
   const todoDeleteId = req.body.todoId;
   todos = todos.filter((todo) => todo.id !== todoDeleteId);
-  todos.forEach((todo) => console.log(todo.id));
-  console.log("comparison", todoDeleteId);
   res.redirect("/");
 });
 
 app.get("/edit/:id", (req, res) => {
   const todoEdit = todos.find((todo) => todo.id == req.params.id);
-  console.log(todoEdit);
   res.render("edit", { todo: todoEdit });
 });
 
 app.post("/edit/:id", (req, res) => {
-  console.log(req.params.id);
   const newTodo = {
     id: req.params.id,
     title: req.body.todo_title,
@@ -66,3 +61,41 @@ app.post("/edit/:id", (req, res) => {
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+/* SE SI VOLESSE UTILIZZARE MONGODB */
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://NOMEDATABASE", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+//
+// const todoSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   description: { type: String },
+//   state: { type: String },
+//   deadline: { type: Date, default: Date.now },
+// });
+//
+// const ToDo = mongoose.model('ToDo', todoSchema);
+//
+// TO GET TODOS
+// const todos = await ToDo.find().sort({ createdAt: "desc" });
+//
+// TO CREATE A NEW TODO
+// const newTodo = new ToDo({
+// DATI RELATIVI AL TODO
+// });
+// try {
+//   await newTodo.save();
+// } catch (error) {
+//   res.status(400);
+// }
+//
+// TO EDIT A TODO
+// const getTodo = await ToDo.findByIdAndUpdate(req.body.id, {
+//   NUOVI DATI RELATIVI AL TODO
+// });
+//
+// TO DELETE A TODO
+// const deleteTodo = await ToDo.findByIdAndRemove(req.body.id);
