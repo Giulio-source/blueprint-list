0940b25e3443f1a2e176ad2a0d2ff1988b805b30
Giulio Poggia <giulio.poggia@gmail.com>
2021-03-31 15:45:09 +0200

    prepared for heroku

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post("/edit/:id", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
 
